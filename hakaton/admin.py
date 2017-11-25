f66aa57dc88ec189490dbe3dd6dba0b52d2bc068
Andrej Karadzic <andrej.karadzic@example.com>
2017-11-25 12:12:34 +0100

    upsell_event_helper hotfixes, plan_data serializing, admin

--- hakaton/admin.py
+++ hakaton/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Customer, FlightPriceCache
+from .models import Customer, FlightPriceCache, Plan, HotelCache, HotelPriceCache
 
 admin.site.register(Customer)
+admin.site.register(Plan)
 admin.site.register(FlightPriceCache)
+admin.site.register(HotelPriceCache)
+admin.site.register(HotelCache)
