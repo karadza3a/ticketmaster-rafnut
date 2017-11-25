4ab88395d60b45241feb2e27f2a2b18716194cf6
Andrej Karadzic <andrej.karadzic@example.com>
2017-11-25 08:11:01 +0100

    real_events uncluttered

--- hakaton/apis/ticketmaster.py
+++ hakaton/apis/ticketmaster.py
@@ -1,12 +1,11 @@
-from datetime import timedelta,datetime
-
+from datetime import timedelta, datetime
 
 from django.utils.datetime_safe import datetime
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from hakaton.models import Customer
 
 from hakaton.apis import api
+from hakaton.models import Customer
 
 time_format = "%Y-%m-%dT%H:%M:%SZ"
 
@@ -41,7 +40,7 @@ def real_events(request):
     start_date_time = datetime.strptime(request.GET["startTime"], "%Y-%m-%d")
     end_date_time = datetime.strptime(request.GET["endTime"], "%Y-%m-%d")
 
-    all_events = []
+    all_bands = []
     for like in likes:
         params = {'keyword': like,
                   'classificationName': "Music",
@@ -51,8 +50,48 @@ def real_events(request):
 
         result = api.ticketmaster_api_call(params)
         if '_embedded' in result:
-            all_events = all_events + result['_embedded']['events']
+            all_bands.append({
+                'events': result['_embedded']['events'],
+                'name': like
+            })
 
-    return Response(all_events)
+    for band in all_bands:
+        for event in band['events']:
+            maxw = 0
+            for image in event['images']:
+                if image['width'] > maxw:
+                    maxw = image['width']
+                    event['image'] = image
+            event.pop('images', None)
 
+            event.pop('seatmap', None)
+            event.pop('test', None)
+            event.pop('promoters', None)
+            event.pop('classifications', None)
+            event.pop('info', None)
+            event.pop('pleaseNote', None)
+            event.pop('sales', None)
+            event.pop('promoters', None)
+            event.pop('promoter', None)
+            event.pop('ada', None)
 
+            event['datetime'] = event['dates']["start"]["dateTime"]
+            event.pop('dates', None)
+
+            venue = event['_embedded']['venues'][0]
+            venue.pop('boxOfficeInfo', None)
+            venue.pop('parkingDetail', None)
+            venue.pop('accessibleSeatingDetail', None)
+            venue.pop('generalInfo', None)
+            venue.pop('upcomingEvents', None)
+            venue.pop('ada', None)
+            venue.pop('markets', None)
+            venue.pop('dmas', None)
+            venue.pop('test', None)
+            venue.pop('images', None)
+            event['venue'] = venue
+            event.pop('_embedded')
+
+    print(all_bands)
+
+    return Response(all_bands)
